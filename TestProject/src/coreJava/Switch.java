5c78693cf24929b7ef547cb71f5bbbbf15c35b74
usew <usew@DESKTOP-KDJE53P>
2022-03-09 19:39:55 +0530

    Switch Flow

---
+++ TestProject/src/coreJava/Switch.java
@@ -0,0 +1,34 @@
+package coreJava;
+
+public class Switch {
+
+	public static void main(String[] args) {
+
+		int x= 10;
+		
+		switch (x)
+		
+		{
+		default:
+			
+			System.out.println("default statement");
+			
+		case 0:
+			
+			System.out.println(0);
+			 break;
+			 
+		case 1:
+			
+			System.out.println(1);
+			
+		case 2:
+			
+			System.out.println(2);
+			
+			
+		}
+	}
+		
+
+}
